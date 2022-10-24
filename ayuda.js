0c5e4bf6407da343a4b4790c3c78307ab1cbf6d1
Anibal Morales <morales.anibal.enrique@gmail.com>
2022-10-24 20:23:45 -0300

    links funcionales y arreglos de css

--- ayuda.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.bluelytics.com.ar/v2/latest')
-    .then(res => res.json ())
-    .then(data => {
-        //console.log(data.results)
-        data.results.forEach(element => {
-            console.log(element.name)
-        })
-    })
-    .catch(error => console.log(error))
